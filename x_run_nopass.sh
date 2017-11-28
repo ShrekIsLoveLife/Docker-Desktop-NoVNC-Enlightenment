48f65687648458186b01cb627001d7d7308581ae
Shrek Is Love <shrek@shrekislove.gq>
2017-11-28 02:57:54 -0500

    adjusted sample mount points

--- x_run_nopass.sh
+++ x_run_nopass.sh
@@ -1,1 +1,1 @@
-docker run --rm -i -p 5901:5901 -p 8082:80 -p 8443:443 -v /opt/storage2/docker_dev/v/:/home/user/Downloads/ shrek/ubuntu-openbox-tigervnc-novnc-firefox-esr 
+docker run --rm -i -p 5901:5901 -p 8082:80 -p 8443:443 -v /home/shrek/Downloads/:/home/user/Downloads/ shrek/ubuntu-openbox-tigervnc-novnc-firefox-esr 
