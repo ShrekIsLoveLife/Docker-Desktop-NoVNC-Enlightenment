acc1f8ecc01883b23367bf205a71f1992170df71
Shrek Is Love <shrek@shrekislove.gq>
2017-12-12 22:07:18 -0500

    clean up the old naming cleanup in build

--- enlightenment_base/build_docker.sh
+++ enlightenment_base/build_docker.sh
@@ -1,5 +1,3 @@
-docker rmi shrek/ubuntu-enlightment
-docker rmi shrek/ubuntu-enlightment-base
 docker rmi shrek/ubuntu-enlightenment
 docker rmi shrek/ubuntu-enlightenment-base
 docker build --rm -t "shrek/ubuntu-enlightenment-base" .
